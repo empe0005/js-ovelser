1cf6c1585886d59dbe86f4346480654b56169320
Emma <empe0005@stud.kea.dk>
2024-08-28 17:47:57 +0200

    first try

---
+++ script.js
@@ -0,0 +1,16 @@
+const fanta = document.querySelector("#fanta");
+fanta.addEventListener("click", KnapKlik);
+function KnapKlik() {
+  console.log("du har klikket på knappen");
+}
+const snaps = document.querySelector("#snaps");
+snaps.addEventListener("click", KnapKlik2);
+function KnapKlik2() {
+  console.log("du har klikket på knappen");
+}
+
+if () {
+  console.log("alkoholfri");
+} else if ((drik = snaps)) {
+  console.log("indeholder alkohol");
+}
